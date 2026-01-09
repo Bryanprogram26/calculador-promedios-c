867bd080528c6bcdc3f4e7e9f73c28e7184c157a
Bryanprogram26 <Bryanarana2606@gmail.com>
2026-01-09 16:31:12 -0600

    agregue validacion de notas con while y cambie los valores de int a float para ser mas preciso

--- promedio.c
+++ promedio.c
@@ -1,41 +1,41 @@
 #include <stdio.h>
 
 int main () {
-	int calificacion1, calificacion2, calificacion3, suma, total;
-	printf("bienvenido a la calculadora de promedios \t ingresa tus calificaciones en numeros del 1-100\n");
+	float calificacion1, calificacion2, calificacion3, suma, total;
+	printf("bienvenido a la calculadora de promedios \n ingresa tus calificaciones en numeros del 0-10\n");
 	printf("ingresa tu primer nota:");
-	scanf("%d", &calificacion1);
-	if (calificacion1 < 0 || calificacion1 > 100) {
-		printf("solo numeros del 0-100\n");
-		printf("ingresa ru primer nota:");
-		scanf("%d", &calificacion1);
+	scanf("%f", &calificacion1);
+	while (calificacion1 < 0 || calificacion1 > 10) {
+		printf("!Error! la nota debe estar entre 0 y 10.\n");
+		printf("intenta nuevamente:");
+		scanf("%f", &calificacion1);
 	}
 	printf("ingresa tu segunda nota:");
 	scanf("%d", &calificacion2);
-	if (calificacion2 < 0 || calificacion2 > 100) {
-		printf("solo numeros del 0-100\n");
-		printf("ingresa tu segunda nota:");
-		scanf("%d", &calificacion2);
+	while (calificacion2 < 0 || calificacion2 > 10) {
+		printf("!Error! la nota debe estar entre 0 y 10.\n");
+		printf("intenta nuevamente:");
+		scanf("%f", &calificacion2);
 	}
 	printf("ingresa tu tercer nota:");
-	scanf("%d", &calificacion3);
-	if (calificacion3 < 0 || calificacion3 > 100) {
-		printf("solo numeros del 0-100\n");
-		printf("ingresa tu tercer nota:");
-		scanf("%d", &calificacion3);
+	scanf("%f", &calificacion3);
+	while (calificacion3 < 0 || calificacion3 > 10) {
+		printf("!Error! la nota debe estar entre 0 y 10.\n");
+		printf("intenta nuevamente:");
+		scanf("%f", &calificacion3);
 	}
 	suma = calificacion1 + calificacion2 + calificacion3;
 	total = suma / 3;
-	printf("tu calificacion es: %d\n", total);
-	if (total == 100) {
+	printf("tu calificacion es: %.2f\n", total);
+	if (total == 10) {
 		printf("increible,calificacion perfecta");
-	} else if (total >= 90) {
+	} else if (total >= 9) {
 		printf(" muy bien, casi perfecto");
-	} else if (total < 90 && total >= 80) {
+	} else if (total < 9 && total >= 8) {
 		printf("lo hiciste bien, sigue asi");
-	} else if (total < 80 && total >= 70) {
+	} else if (total < 8 && total >= 7) {
 		printf("aprobaste pero puede mejorar");
-	} else if (total < 70 && total >= 60) {
+	} else if (total < 7 && total >= 6) {
 		printf("aprobaste, pero tienes que mejorar");
 	} else {
 		printf("reprobaste, tienes que estudiar mas");
