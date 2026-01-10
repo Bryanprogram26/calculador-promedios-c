22d81a24b06e1513445a2e4bd5822b16389057ef
Bryanprogram26 <Bryanarana2606@gmail.com>
2026-01-09 21:38:12 -0600

    es la version final de un programa que te dice las tablas de multiplicar que quieras del numero que quieras, aguegue las funciones de que se pueda usar varias veces el programa y poder elegir cuantas tablas del numero elegido mostrar

--- tablas.c
+++ tablas.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 int main() {
-	int n,;
+	int n, repetir, i2;
+	do {
 	printf("Bienvenido a las tablas de multiplicar\n");
 	printf("que tabla quieres conocer:");
 	scanf("%d", &n);
-	for(int i=1; i <= 10; i++) {
+	printf("cuantas tablas quieres conocer:");
+	scanf("%d", &i2);
+	for(int i=1; i <= i2; i++) {
 		printf("%d x %d = %d\n", n, i, n*i);
-	}
+	} printf("quieres volver a usar el programa? (1 para si y 0 para no) ");
+	scanf("%d", &repetir);
+	} while (repetir == 1);
 	return 0;
 }
